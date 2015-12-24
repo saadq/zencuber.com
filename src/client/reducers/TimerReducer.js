3bd1f3a710057378b5179694607cd510c41d0636
saadq <saad@saadquadri.com>
2015-12-24 16:46:22 -0500

    Add TimerReducer

---
+++ src/client/reducers/TimerReducer.js
@@ -0,0 +1,41 @@
+import { formatTime } from '../../util'
+import * as types  from '../constants/TimerActions'
+
+const initialState = {
+  isOn: false,
+  time: formatTime(0)
+}
+
+const TimerReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.START_TIMER:
+      return {
+        ...state,
+        isOn: true,
+        offset: Date.now()
+      }
+
+    case types.STOP_TIMER:
+      return {
+        ...state,
+        isOn: false
+      }
+
+    case types.RESET_TIMER:
+      return {
+        ...state,
+        time: formatTime(initialState.time)
+      }
+
+    case types.TICK:
+      return {
+        ...state,
+        time: formatTime(state.time + action.time)
+      }
+
+    default:
+      return state
+  }
+}
+
+export default TimerReducer
