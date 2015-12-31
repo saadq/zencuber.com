f5825a251d917cbeae9774da3703d5ac23268a14
saadq <saad@saadquadri.com>
2015-12-31 02:21:56 -0500

    Add livereactload

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,88 +1,96 @@
 import gulp from 'gulp'
-import gulpif from 'gulp-if'
-import source from 'vinyl-source-stream'
+import gutil from 'gulp-util'
 import sass from 'gulp-sass'
+import nodemon from 'gulp-nodemon'
+import source from 'vinyl-source-stream'
+import buffer from 'vinyl-buffer'
 import browserify from 'browserify'
-import babelify from 'babelify'
 import watchify from 'watchify'
-import livereload from 'gulp-livereload'
+import babelify from 'babelify'
+import envify from 'envify'
+import livereactload from 'livereactload'
 import eslint from 'gulp-eslint'
 import del from 'del'
 
 const jsPaths = {
-  'src': 'src/client/index.js',
-  'dest': 'dist/js'
+  src: './src/client/index.js',
+  dest: './dist/js'
 }
 
 const sassDirs = {
-  src: 'src/client/styles/main.sass',
-  dest: 'dist/css'
+  src: './src/client/styles/main.sass',
+  dest: './dist/css'
 }
 
 const assetPaths = {
-  images: 'src/assets/images/**/*',
-  fonts: 'src/assets/font/**/*',
-  jquery: 'src/assets/vendor/jquery/jquery-1.11.3.js',
+  images: './src/client/assets/images/**/*',
+  fonts: './src/client/assets/font/**/*',
+  jquery: './src/client/assets/vendor/jquery/jquery-1.11.3.js',
   materialize: {
-    css: 'src/assets/vendor/materialize/css/materialize.css',
-    js: 'src/assets/vendor/materialize/js/materialize.js'
+    css: './src/client/assets/vendor/materialize/css/materialize.css',
+    js: './src/client/assets/vendor/materialize/js/materialize.js'
   }
 }
 
-let watch
+let isProd = process.env.NODE_ENV === 'production'
+  , watch = false
 
-function browserifyScript() {
-  let b = browserify({
+function createBundler() {
+  let bundler = browserify({
+    entries: [jsPaths.src],
+    transform: [ [babelify, {}], [envify, {}] ],
+    plugin: (isProd || !watch) ? [] : [ livereactload ],
+    debug: !isProd,
     cache: {},
     packageCache: {},
-    fullPaths: true,
-    debug: true
+    fullPaths: !isProd
   })
-
-  b = b.transform('babelify', { presets: ['es2015', 'react'] })
-
-  if (watch) {
-    b = watchify(b)
-    b.on('update', () => { bundle(b) })
-  }
-
-  b.add(jsPaths.src)
-  bundle(b)
+  return bundler
 }
 
-function bundle(b) {
-  let start = Date.now()
-  b.bundle()
-    .on('error', (err) => { console.error(err.stack) })
-    .on('end', () => {
-      let end = Date.now()
-      let totalTime = end - start
-      console.log(`finished scripts in ${totalTime} ms`)
-    })
+gulp.task('scripts', () =>  {
+  watch = false
+  createBundler()
+    .bundle()
     .pipe(source('bundle.js'))
     .pipe(gulp.dest(jsPaths.dest))
-    .pipe(gulpif(watch, livereload()))
-}
-
-gulp.task('lint', function () {
-  return gulp.src(['src/**/*.js', '!node_modules/**', '!**/assets/**'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
 })
 
-gulp.task('scripts', () => {
-  watch = false
-  browserifyScript()
-})
-
-gulp.task('watch-scripts', () => {
+gulp.task('watch-scripts', () =>  {
   watch = true
-  browserifyScript()
+  let watcher = watchify(createBundler())
+  rebundle()
+  return watcher
+    .on('error', gutil.log)
+    .on('update', rebundle)
+
+  function rebundle() {
+    gutil.log('Update JavaScript bundle')
+    watcher
+    .bundle()
+    .on('error', gutil.log)
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest(jsPaths.dest))
+  }
+})
+
+gulp.task('watch-server', () =>  {
+  nodemon({
+    script: './src/server/index.js',
+    exec: './node_modules/.bin/babel-node',
+    ext: 'js',
+    ignore: ['gulpfile.js', 'bundle.js', 'node_modules/*']
+  })
+  .on('change', [])
+  .on('restart',  () =>  {
+    console.log('Server restarted')
+  })
 })
 
 gulp.task('styles', () => {
-  gulp.src(sassDirs.src)
+  gulp
+    .src(sassDirs.src)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(sassDirs.dest))
 })
@@ -91,22 +99,34 @@ gulp.task('watch-styles', () => {
   gulp.watch('src/client/styles/**/*.sass', ['styles'])
 })
 
+gulp.task('lint', () => {
+  return gulp
+    .src(['src/**/*.js', '!node_modules/**', '!**/assets/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+})
+
 gulp.task('copy', () => {
-  gulp.src(assetPaths.fonts)
+  gulp
+    .src(assetPaths.fonts)
     .pipe(gulp.dest('dist/font'))
 
-  gulp.src(assetPaths.images)
+  gulp
+    .src(assetPaths.images)
     .pipe(gulp.dest('dist/img'))
 
-  gulp.src([assetPaths.jquery, assetPaths.materialize.js])
+  gulp
+    .src([assetPaths.jquery, assetPaths.materialize.js])
     .pipe(gulp.dest('dist/js'))
 
-  gulp.src([assetPaths.materialize.css])
+  gulp
+    .src([assetPaths.materialize.css])
     .pipe(gulp.dest('dist/css'))
 })
 
 gulp.task('clean', () => { del(['dist']) })
 gulp.task('build', ['lint', 'scripts', 'styles', 'copy'])
-gulp.task('watch', ['watch-scripts', 'watch-styles'])
+gulp.task('watch', ['watch-server', 'watch-scripts', 'watch-styles'])
 
 gulp.task('default', ['build'])
