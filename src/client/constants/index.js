fcfedfcdb721729f69f39ca144f78b07cb978182
saadq <saad@saadquadri.com>
2016-01-18 19:49:02 -0500

    Add constants for recent times box

--- src/client/constants/index.js
+++ src/client/constants/index.js
@@ -1,3 +1,6 @@
 export const START_TIMER = 'START_TIMER'
 export const STOP_TIMER = 'STOP_TIMER'
 export const RESET_TIMER = 'RESET_TIMER'
+export const ADD_TIME = 'ADD_TIME'
+export const REMOVE_TIME = 'REMOVE_TIME'
+export const CLEAR_TIMES = 'CLEAR_TIMES'
