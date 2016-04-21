a6870f95a15678b88118dbc7f302d82f73957047
saadq <saad@saadquadri.com>
2016-04-21 17:28:28 -0400

    Remove leftover breakpoint code

--- src/client/actions/SettingsActions.js
+++ src/client/actions/SettingsActions.js
@@ -1,15 +1,8 @@
-import { CHANGE_MODE, CHANGE_INSPECTION } from '../constants'
+import { CHANGE_MODE } from '../constants'
 
 export function changeMode(mode) {
   return {
     type: CHANGE_MODE,
     mode
-  }
-}
-
-export function changeInspection(inspectionTime) {
-  return {
-    type: CHANGE_INSPECTION,
-    inspectionTime
   }
 }
