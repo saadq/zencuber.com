1494e8718fbf2d183b2e21b0261bc48b467e49b8
Saad Quadri <saad@saadquadri.com>
2017-06-13 08:04:57 -0400

    add prod webpack config

---
+++ client/webpack.prod.config.js
@@ -0,0 +1,63 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    './src/index.js'
+  ],
+
+  output: {
+    filename: 'static/bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [
+          'babel-loader'
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.styl$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader?modules&importLoaders=1!stylus-loader',
+          fallback: 'style-loader'
+        })
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|ico|svg|pdf|ttf|eof|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      comments: false
+    }),
+    new ExtractTextPlugin('static/style.css'),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: 'body',
+      favicon: 'src/assets/favicon.ico'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  ]
+}
