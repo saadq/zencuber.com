bdcde000cc92684fe3d7daeaca094096c4d55f09
Saad Quadri <saad@saadq.com>
2017-08-30 02:13:07 -0400

    add comments back to Timer

--- client/src/components/home/Timer.js
+++ client/src/components/home/Timer.js
@@ -43,15 +43,34 @@ class Timer extends Component {
   props: Props
   interval: *
 
+  /**
+   * Sets up the keyboard listeners for the timer.
+   */
+
   componentDidMount() {
     window.addEventListener('keyup', this.onKeyUp)
     window.addEventListener('keydown', this.onKeyDown)
   }
 
+  /**
+   * Remove the keypress listeners when the component unmounts.
+   */
+
   componentWillUnmount() {
     window.removeEventListener('keyup', this.onKeyUp)
     window.removeEventListener('keydown', this.onKeyDown)
   }
+
+  /**
+   * Runs anytime a key is pressed.
+   *
+   * If the timer is already running, it stops.
+   * If the timer isn't running and it isn't "paused", it starts initialization.
+   *
+   * Initialization takes 350ms to complete.
+   *
+   * @param e The event object.
+   */
 
   onKeyDown = (e: SyntheticKeyboardEvent) => {
     const { status, initializeTimer, updateScramble } = this.props
@@ -64,6 +83,16 @@ class Timer extends Component {
       initializeTimer()
     }
   }
+
+  /**
+   * Runs anytime a key is released.
+   *
+   * If the timer was paused, unpause it.
+   * If the timer hasn't finished initializing, cancel initialization.
+   * If the timer is fully initialized, start the timer.
+   *
+   * @param e The event object.
+   */
 
   onKeyUp = (e: SyntheticKeyboardEvent) => {
     const { status, unpauseTimer, cancelTimerInitialization } = this.props
@@ -84,6 +113,14 @@ class Timer extends Component {
     }
   }
 
+  /**
+   * Starts the timer.
+   *
+   * forceUpdate() is needed to constantly rerender the timer because the
+   * state isn't changing as time passes, so we need to tell it to manually
+   * calculate how much time has passed and rerender it with that.
+   */
+
   start = () => {
     const { status, startTimer } = this.props
 
@@ -94,6 +131,17 @@ class Timer extends Component {
     startTimer(Date.now())
     this.interval = setInterval(() => this.forceUpdate(), 10)
   }
+
+  /**
+   * Stops the timer and generates a new scramble.
+   *
+   * The timer is set to a "paused" state immediately after the timer stops.
+   * This is to make sure that the timer doesn't immediately start again once
+   * the key that you used to stop it is released.
+   *
+   * Instead of restarting the timer, releasing the key will just unpause it,
+   * and it can be used normally again.
+   */
 
   stop = () => {
     const { status, stopTimer } = this.props
@@ -120,6 +168,17 @@ class Timer extends Component {
     addSolve(solve)
   }
 
+  /**
+   * Formats the elapsed amount of time to a nice-looking string.
+   *
+   * If the timer is running, the format is like this: "32.7".
+   * If the timer isn't running, the format is like this: "32.70".
+   *
+   * @param elapsed The amount of time that has passed in milliseconds.
+   *
+   * @return The amount of time that has passed.
+   */
+
   timeFormatter(elapsed: number): string {
     const time = new Date(elapsed)
     const seconds = time.getSeconds().toString()
@@ -128,12 +187,23 @@ class Timer extends Component {
     return `${seconds}.${milliseconds}`
   }
 
+  /**
+   * Calculates how much time has passed since the timer has started.
+   * If the timer isn't running, just return 0.
+   *
+   * @return The elapsed time.
+   */
+
   getElapsedTime(): number {
     const { startTime, stopTime } = this.props
     const elapsed = startTime ? (stopTime || Date.now()) - startTime : 0
 
     return elapsed
   }
+
+  /**
+   * Display the formatted time.
+   */
 
   render() {
     const { status } = this.props
